de6487efae7508f60d4978758c0150b52fc670ae
Victor Okoro <vicstrokes1@gmail.com>
2023-03-07 14:31:08 +0100

    my First Server development

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 
 require('dotenv').config()
@@ -7,8 +8,11 @@ const port = process.env.PORT || 8000
 
 http.createServer(
     (req, res) => {
-        res.writeHead(200, { 'Content-Type': 'text/plain' })
-        res.write("Hello! Nigeria!")
-        res.end()
+        fs.readFile("about.html", (err, data) => {
+            res.writeHead(200, { 'Content-Type': 'text/plain' })
+            res.write("Hello! Nigeria!")
+            res.write(data)
+            res.end()
+        })
     }
 ).listen(port, () => console.log("Server established! "))
